4023b3273fc89f9ac03eed65a0a0ccdd68959fa1
chenjing <JYcj*0921b>
2017-08-30 16:27:41 +0800

    Initial commit

---
+++ src/main/java/SampleController.java
@@ -0,0 +1,22 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by 陈敬 on 17/8/30.
+ */
+@RestController
+@EnableAutoConfiguration
+public class SampleController {
+
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleController.class, args);
+    }
+}
