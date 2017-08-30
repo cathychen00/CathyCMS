f0f7973689129d76bf4b20ec5ceb976f9cc5a16d
chenjing <JYcj*0921b>
2017-08-30 16:57:18 +0800

    初始化spring boot站点结构

---
+++ src/main/java/com/cathycms/Application.java
@@ -0,0 +1,16 @@
+package com.cathycms;
+
+import com.cathycms.web.SampleController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by 陈敬 on 17/8/30.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
--- src/main/java/com/cathycms/web/SampleController.java
+++ src/main/java/com/cathycms/web/SampleController.java
@@ -1,3 +1,5 @@
+package com.cathycms.web;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,16 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by 陈敬 on 17/8/30.
  */
 @RestController
-@EnableAutoConfiguration
 public class SampleController {
-
-
     @RequestMapping("/")
     String home() {
         return "Hello World!";
-    }
-
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(SampleController.class, args);
     }
 }
