5f91d1ca74748d27ccf4cde8d163303c63e1eae8
chenjing <JYcj*0921b>
2017-09-11 18:15:13 +0800

    +shiro还没调试好

--- CathyCMS-admin/src/main/java/com/cathy/cms/Application.java
+++ CathyCMS-admin/src/main/java/com/cathy/cms/Application.java
@@ -1,7 +1,9 @@
 package com.cathy.cms;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
@@ -9,7 +11,8 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 /**
  * Created by 陈敬 on 17/8/30.
  */
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = { "com.cathy.cms" })
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) throws Exception {
---
+++ CathyCMS-admin/src/main/java/com/cathy/cms/config/ShiroConfig.java
@@ -0,0 +1,77 @@
+package com.cathy.cms.config;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import com.cathy.cms.shiro.UserRealm;
+import org.apache.shiro.cache.ehcache.EhCacheManager;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.session.SessionListener;
+import org.apache.shiro.session.mgt.SessionManager;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by 陈敬 on 17/9/11.
+ */
+@Configuration
+public class ShiroConfig {
+    /**
+     * ShiroFilterFactoryBean 处理拦截资源文件问题。
+     * 注意：单独一个ShiroFilterFactoryBean配置是或报错的
+     * 初始化ShiroFilterFactoryBean的时候需要注入：SecurityManager
+     */
+    @Bean
+    public ShiroFilterFactoryBean shirFilter(SecurityManager securityManager) {
+        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+
+        // 必须设置 SecurityManager
+        shiroFilterFactoryBean.setSecurityManager(securityManager);
+
+        // 拦截器.
+        Map<String, String> filterChainDefinitionMap = new LinkedHashMap<String, String>();
+
+        //filterChainDefinitionMap.put("/page/*", "authc");
+        // 配置退出过滤器,其中的具体的退出代码Shiro已经替我们实现了
+        //filterChainDefinitionMap.put("/security/logoff", "logout");
+
+        shiroFilterFactoryBean.setLoginUrl("/login");
+        // 未授权界面;
+        shiroFilterFactoryBean.setUnauthorizedUrl("/403");
+
+        shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainDefinitionMap);
+
+        return shiroFilterFactoryBean;
+    }
+
+    @Bean
+    public SecurityManager securityManager() {
+        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
+        UserRealm myRealm = new UserRealm();
+        securityManager.setRealm(myRealm);
+        securityManager.setSessionManager(sessionManager());
+        securityManager.setCacheManager(ehCacheManager());
+        return securityManager;
+    }
+
+    @Bean
+    public SessionManager sessionManager() {
+        DefaultWebSessionManager sessionManager = new DefaultWebSessionManager();
+        Collection<SessionListener> listeners = new ArrayList<SessionListener>();
+//        listeners.add(new MySessionListener());
+        sessionManager.setSessionListeners(listeners);
+        return sessionManager;
+    }
+
+    @Bean
+    public EhCacheManager ehCacheManager() {
+        EhCacheManager ehCacheManager = new EhCacheManager();
+        ehCacheManager.setCacheManagerConfigFile("classpath:Ehcache.xml");
+        return ehCacheManager;
+    }
+}
