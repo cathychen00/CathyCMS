f4d244cd0192fda751da5efdcbb1ebc560f09449
chenjing <JYcj*0921b>
2017-09-14 11:04:21 +0800

    试一下读取session信息

--- CathyCMS-adminsite/src/main/java/com/cathy/cms/controller/UserController.java
+++ CathyCMS-adminsite/src/main/java/com/cathy/cms/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.cathy.cms.controller;
 
+import com.cathy.cms.utils.WebHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * Created by 陈敬 on 17/9/12.
@@ -14,5 +17,11 @@ public class UserController {
     @RequestMapping("/list")
     public String list(Model model){
         return "/user/list";
+    }
+
+    @RequestMapping("/update_pwd")
+    public String updatePassword(Model model, HttpSession session){
+        session.setAttribute("test1","test1");
+        return "/user/updatePwd";
     }
 }
